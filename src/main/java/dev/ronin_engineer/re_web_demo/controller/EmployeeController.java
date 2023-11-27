ce77c2d256dbd4adc2c9cfff9e40310b301f5641
vuong <vuong.lv34@gmail.com>
2023-11-27 22:09:46 +0700

    add get employees

--- src/main/java/dev/ronin_engineer/re_web_demo/controller/EmployeeController.java
+++ src/main/java/dev/ronin_engineer/re_web_demo/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package dev.ronin_engineer.re_web_demo.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
 
+    @GetMapping
+    public String getEmployees(){
+        return null;
+    }
 }
